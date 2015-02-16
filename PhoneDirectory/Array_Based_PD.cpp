dc4bae66597182d9788cdcd180383afc4113cb7b
kdelude16 <kdelude16@my.whitworth.edu>
2015-02-16 15:18:30 -0800

    Step 6 complete

--- PhoneDirectory/Array_Based_PD.cpp
+++ PhoneDirectory/Array_Based_PD.cpp
@@ -115,7 +115,14 @@ void Phone_Directory::save()
 	*/
 string Phone_Directory::remove_entry(const string& name) // Exercise 1.7: please complete the remove_entry() method - Ed/Kent
 {
-
+	if (find(name) >= 0){
+		int index = find(name);
+		string numb = the_directory[index].get_number();
+		 for (int i = index; i < size - 1; i++)
+			the_directory[i] = the_directory[i + 1];
+		 return numb;
+	}
+	else return "";
 	// Hint: you can use the code below to shift names down in the directory to remove the selected entry specified by "index"
 	// for (int i = index; i < size - 1; i++)
 		// the_directory[i] = the_directory[i + 1];
